cf5d4a226aeb4afc6a686a4864f5a9be0ae2218d
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2023-11-10 23:25:42 +0100

    fix:hero anim

--- client/src/Components/HeroSection/HeroSection.js
+++ client/src/Components/HeroSection/HeroSection.js
@@ -4,41 +4,9 @@ import Button from "../Button/Button";
 import { gsap } from "gsap";
 
 const HeroSection = () => {
-  const heroContainerRef = useRef();
-
-  useEffect(() => {
-    const heroTL = gsap.timeline();
-    const headerTitle = heroContainerRef.current.firstElementChild.children;
-    const pText = heroContainerRef.current.firstElementChild.nextSibling.children
-    const btn = heroContainerRef.current.children[2]
-    console.log(btn);
-
-    const ctx = gsap.context(() => {
-      heroTL.from(headerTitle, {
-        y: 150,
-        duration: 1,
-        ease: "power3",
-        delay: 2,
-      })
-      .from(pText, {
-        autoAlpha:0,
-        y: 100,
-        duration: 1,
-        ease: "power3",
-      }, '-=0.6')
-      .from(btn, {
-        autoAlpha:0,
-        duration: 1,
-        ease: "power3",
-      }, '-=0.4')
-    });
-
-    return () => ctx.revert();
-  });
-
   return (
     <div className="hero-imgae">
-      <section className="hero" ref={heroContainerRef}>
+      <section className="hero">
         <span className="hiden-container">
           <h1 className="hero-title">
             Exquisite dining <br /> since 1989
