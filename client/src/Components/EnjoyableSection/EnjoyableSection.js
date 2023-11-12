62bb07af66dce7ff56067c74942ec9aee8035ea6
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2023-11-12 14:14:39 +0100

    feat:animation added to enjoyable section on-scroll

--- client/src/Components/EnjoyableSection/EnjoyableSection.js
+++ client/src/Components/EnjoyableSection/EnjoyableSection.js
@@ -1,18 +1,103 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./EnjoyableSection.scss";
 import hills from "../../assets/images/homepage/enjoyable-place-desktop.jpg";
 import hillsT from "../../assets/images/homepage/enjoyable-place-tablet.jpg";
 import hillsM from "../../assets/images/homepage/enjoyable-place-mobile.jpg";
 import svgIcon from "../../assets/images/patterns/pattern-divide.svg";
+import { gsap } from "gsap";
 
 const EnjoyableSection = () => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef(null);
+  const mainRef = useRef();
+  
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0].isIntersecting;
+        setIsIntersecting(entry);
+      },
+      { threshold: 0.6 }
+    );
+    observer.observe(mainRef.current);
+    return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
+    const pic_1 = mainRef.current.children[0];
+    const pic_2 = mainRef.current.children[1];
+    const pic_3 = mainRef.current.children[2];
+    const pic_4 = mainRef.current.children[3];
+    // console.log(pic);
+    const svg = ref.current.children[0];
+    const tilte = ref.current.children[1];
+    const pText = ref.current.children[2];
+    const timeline = gsap.timeline();
+    // console.log(pic);
+    if (isIntersecting) {
+      timeline
+        .to(
+          pic_1,
+          {
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+            duration: 2.4,
+            ease: "power2.easeInOut",
+          },
+          "<"
+        )
+        .to(
+          pic_2,
+          {
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+            duration: 2.4,
+            ease: "power2.easeInOut",
+          },
+          "<"
+        )
+        .to(
+          pic_3,
+          {
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+            duration: 2.4,
+            ease: "power2.easeInOut",
+          },
+          "<"
+        )
+        .to(
+          pic_4,
+          {
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+            duration: 2.4,
+            ease: "power2.easeInOut",
+          },
+          "<"
+        )
+        .to(svg, {
+          autoAlpha: 1,
+          x: -0,
+          duration: 0.7,
+          ease: "power2",
+        })
+        .to(tilte, {
+          autoAlpha: 1,
+          x: 0,
+          duration: 0.7,
+        })
+        .to(pText, {
+          autoAlpha: 1,
+          x: 0,
+          duration: 0.7,
+        });
+    }
+  }, [isIntersecting]);
+
   return (
-    <section className="enjoyable">
+    <section className="enjoyable" ref={mainRef}>
       <img src={hills} alt="" className="left-image" />
       <img src={hills} alt="" className="left-image-tablet" />
       <img src={hillsT} alt="" className="left-image-m" />
       <img src={hillsM} alt="" className="left-image-m2" />
-      <div className="right-content">
+      <div className="right-content" ref={ref}>
         <img src={svgIcon} alt="" />
         <h2 className="enjoyable-title">
           Enjoyable place <br /> for all the family
